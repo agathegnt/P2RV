631ad7f70301accb0a13cd3d87eaf9690f52f389
Agathe <aga.guenot@gmail.com>
2018-02-09 17:52:34 +0100

    Test si deux segments sont paralleles

--- P2RV-dessin au schema/P2RV-dessin au schema/perpendiculaire.cpp
+++ P2RV-dessin au schema/P2RV-dessin au schema/perpendiculaire.cpp
@@ -18,6 +18,25 @@ bool IsPerpendicular (Segment f1, Segment f2)
   return perpendicular;
 }
 
+bool IsParallel (Segment f1, Segment f2)
+{
+  bool parallel = false;
+
+  //calcul du produit scalaire
+  Point vector1 = f1.getextremite()-f1.getorogine();
+  Point vector2 = f2.getextremite()-f2.getorogine();
+  int prod_scal = vector1.getx()*vector2.getx() + vector1.gety()*vector2.gety();
+
+  //compare le produit scalaire au produit des normes des vecteurs
+  if (prod_scal - vector1.norme()*vector2.norme()< 0,01)
+  {
+      parallel = true;
+  }
+
+  return parallel;
+
+}
+
 //determine si la forme est fermee ou pas
 bool IsClosed (Trait trait)
 {
